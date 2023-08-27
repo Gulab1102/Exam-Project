d2119d994ee669409589ca5353013656df15442c
Gulab1102 <gk96025@gmail.com>
2023-08-27 15:05:33 +0530

    Exam Project Initilized

--- src/main/java/com/exam/model/User.java
+++ src/main/java/com/exam/model/User.java
@@ -19,10 +19,10 @@ public class User {
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
-    private String username;
+    private String userName;
     private String password;
-    private String firstname;
-    private String lastname;
+    private String firstName;
+    private String lastName;
   
 
 	private String email;
@@ -36,133 +36,109 @@ public class User {
     @OneToMany(cascade = CascadeType.ALL,fetch = FetchType.EAGER,mappedBy = "user")
     @JsonIgnore
     private Set<UserRole> userRole=new HashSet<>();
-    
-    
-    public Set<UserRole> getUserRole() {
-		return userRole;
-	}
-
-
-
-	public void setUserRole(Set<UserRole> userRole) {
-		this.userRole = userRole;
-	}
-
-
 
 	public User() {
-  		
-  	}
+		
+	}
 
-    
-
-	public User(Long id, String username, String password, String firstname, String lastname, String email,
-			String phone, boolean enabled, String profile) {
+	public User(Long id, String userName, String password, String firstName, String lastName, String email,
+			String phone, boolean enabled, String profile, Set<UserRole> userRole) {
 		super();
 		this.id = id;
-		this.username = username;
+		this.userName = userName;
 		this.password = password;
-		this.firstname = firstname;
-		this.lastname = lastname;
+		this.firstName = firstName;
+		this.lastName = lastName;
 		this.email = email;
 		this.phone = phone;
 		this.enabled = enabled;
 		this.profile = profile;
+		this.userRole = userRole;
 	}
-
-
 
 	public Long getId() {
 		return id;
 	}
 
-
 	public void setId(Long id) {
 		this.id = id;
 	}
 
-
-	public String getUsername() {
-		return username;
+	public String getUserName() {
+		return userName;
 	}
 
-
-	public void setUsername(String username) {
-		this.username = username;
+	public void setUserName(String userName) {
+		this.userName = userName;
 	}
-
 
 	public String getPassword() {
 		return password;
 	}
 
-
 	public void setPassword(String password) {
 		this.password = password;
 	}
 
-
-	public String getFirstname() {
-		return firstname;
+	public String getFirstName() {
+		return firstName;
 	}
 
-
-	public void setFirstname(String firstname) {
-		this.firstname = firstname;
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
 	}
 
-
-	public String getLastname() {
-		return lastname;
+	public String getLastName() {
+		return lastName;
 	}
 
-
-	public void setLastname(String lastname) {
-		this.lastname = lastname;
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
 	}
-
 
 	public String getEmail() {
 		return email;
 	}
 
-
 	public void setEmail(String email) {
 		this.email = email;
 	}
-
 
 	public String getPhone() {
 		return phone;
 	}
 
-
 	public void setPhone(String phone) {
 		this.phone = phone;
 	}
-
 
 	public boolean isEnabled() {
 		return enabled;
 	}
 
-
 	public void setEnabled(boolean enabled) {
 		this.enabled = enabled;
 	}
-
 
 	public String getProfile() {
 		return profile;
 	}
 
-
 	public void setProfile(String profile) {
 		this.profile = profile;
 	}
+
+	public Set<UserRole> getUserRole() {
+		return userRole;
+	}
+
+	public void setUserRole(Set<UserRole> userRole) {
+		this.userRole = userRole;
+	}
+	
+	
     
     
-   
     
     
 	
